cc896b74ab718cbec88989543f2d32689b64e7df
thirumeniram Arumugam <thirumeniram25@gmail.com>
2024-03-12 18:09:36 +0530

    started the form project

---
+++ form/form/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  //we have to display the products details -> intially we have to make a request to backend with help of useEffect 
+  //we have to make a get request
+  //fill the data in the list and render it
+  //while form submission we have to make apost request and fill the database and should case a re-render
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
